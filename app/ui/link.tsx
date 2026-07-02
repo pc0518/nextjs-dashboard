e40701d5b7695f44f8f0edc7c077f6d509c0f3d8
pc0518 <pc0518@163.com>
2026-07-02 16:08:40 +0800

    cc14

--- app/ui/link.tsx
+++ app/ui/link.tsx
@@ -1,6 +1,9 @@
 import Link, { LinkProps } from 'next/link'
 
-interface NoPrefetchLinkProps extends LinkProps {
+// 使用 Omit 融合 Next.js 的 LinkProps 和原生 a 标签的 HTML 属性（包含 className、target 等）
+interface NoPrefetchLinkProps 
+  extends LinkProps, 
+    Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> {
   children: React.ReactNode
 }
 
@@ -9,5 +12,6 @@ export default function NoPrefetchLink({
   ...rest 
 }: NoPrefetchLinkProps) {
   
+  // 此时 ...rest 里面完美包含了 prefetch 之外的 className、href 和 children 等所有属性
   return <Link prefetch={prefetch} {...rest} />
 }
